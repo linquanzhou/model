0b9657d34979fdfcb240ef82829e02921b5b24bc
zz <zz@zdeMacBook-Pro.local>
2024-05-03 01:24:00 -0400

    map

--- delg/delg_test/results/test/map.py
+++ delg/delg_test/results/test/map.py
@@ -1,33 +1,30 @@
 import googlemaps
 import folium
 
-api_key = 'AIzaSyCftdIoVGOhvQcu3C6eZVLOLi5bV-qk_NU'
+#api_key = 'your google map api key'
 gmaps = googlemaps.Client(key=api_key)
 
-# 函数用于从文件读取地标名字
+
 def read_landmarks(file_path):
     with open(file_path, 'r') as file:
         landmarks = [line.strip() for line in file.readlines() if line.strip()]
     return landmarks
 
-# 生成地图并标记地点
+
 def create_map(landmarks, output_file='landmark_map.html'):
-    # 如果有可用地标，使用第一个地标的位置初始化地图
     if landmarks:
         first_location = gmaps.geocode(landmarks[0])
         map_location = [first_location[0]['geometry']['location']['lat'], first_location[0]['geometry']['location']['lng']]
     else:
-        # 默认回退位置
-        map_location = [48.8566, 2.3522]  # Paris, France
+        map_location = [48.8566, 2.3522]  
         
     map = folium.Map(location=map_location, zoom_start=12)
     icon_url = 'http://example.com/my_custom_icon.png'
 
-    # 创建一个自定义图标
     custom_icon = folium.CustomIcon(
         icon_image=icon_url,
-        icon_size=(28, 30),  # 设置图标大小
-        icon_anchor=(14, 30)  # 设置图标锚点
+        icon_size=(28, 30),  
+        icon_anchor=(14, 30)  
     )
 
     for landmark in landmarks:
@@ -39,7 +36,7 @@ def create_map(landmarks, output_file='landmark_map.html'):
     map.save(output_file)
     print(f"Map with landmarks saved to {output_file}")
 
-# 主函数
+
 def main():
     landmarks = read_landmarks('unique_labels.txt')
     create_map(landmarks)
