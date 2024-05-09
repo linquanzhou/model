eaeaaed5ea5e5601f848aa7da96c18897b167485
zz <zz@zdeMacBook-Pro.local>
2024-05-08 23:06:07 -0400

    final

---
+++ delg/map.py
@@ -0,0 +1,47 @@
+import googlemaps
+import folium
+
+api_key = 'AIzaSyCftdIoVGOhvQcu3C6eZVLOLi5bV-qk_NU'
+gmaps = googlemaps.Client(key=api_key)
+
+
+def read_landmarks(file_path):
+    with open(file_path, 'r') as file:
+        landmarks = [line.strip() for line in file.readlines() if line.strip()]
+    return landmarks
+
+
+def create_map(landmarks, output_file='landmark_map.html'):
+    if landmarks:
+        first_location = gmaps.geocode(landmarks[0])
+        map_location = [first_location[0]['geometry']['location']['lat'], first_location[0]['geometry']['location']['lng']]
+    else:
+        map_location = [48.8566, 2.3522]  
+        
+    map = folium.Map(location=map_location, zoom_start=12)
+    icon_url = 'http://example.com/my_custom_icon.png'
+
+    custom_icon = folium.CustomIcon(
+        icon_image=icon_url,
+        icon_size=(28, 30),  
+        icon_anchor=(14, 30)  
+    )
+
+    for landmark in landmarks:
+        location = gmaps.geocode(landmark,language='en')
+        if location:
+            latlng = location[0]['geometry']['location']
+            folium.Marker([latlng['lat'], latlng['lng']], popup=landmark, icon=folium.Icon(icon='heart', color='red', prefix='fa')).add_to(map)
+
+    map.save(output_file)
+    print(f"Map with landmarks saved to {output_file}")
+
+
+def main():
+    landmarks = read_landmarks('unique_labels.txt')
+    create_map(landmarks)
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ delg/visualization.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+from PIL import Image
+import matplotlib.pyplot as plt
+from get_landmark_name import load_data
+
+def extract_labels(matches_file):
+    labels = set()
+    with open(matches_file, 'r') as file:
+        for line in file:
+            _, image2_name = line.strip().split(':')
+            label = image2_name.strip().rpartition('_')[0]
+            labels.add(label)
+    return labels
+
+def display_images_with_labels(data_directory, matches_file, results, max_rows=None):
+    row_count = 0
+    with open(matches_file, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        if max_rows is not None and row_count >= max_rows:
+            break
+
+        image1_name, image2_name = line.strip().split(':')
+        image1_name, image2_name = image1_name.strip(), image2_name.strip()
+
+        label1 = 'query'+str(row_count)
+        label2 = results[row_count][1]
+
+        image1_path = os.path.join(data_directory, image1_name + '.jpg')
+        image2_path = os.path.join(data_directory, image2_name + '.jpg')
+
+        try:
+            image1 = Image.open(image1_path)
+            image2 = Image.open(image2_path)
+
+            plt.figure(figsize=(10, 5))
+            plt.subplot(1, 2, 1)
+            plt.imshow(image1)
+            plt.title(f"Label: {label1}")
+            plt.axis('off')
+
+            plt.subplot(1, 2, 2)
+            plt.imshow(image2)
+            plt.title(f"Label: {label2}")
+            plt.axis('off')
+
+            plt.show()
+            row_count += 1
+
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+            continue
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process images and extract labels.")
+    parser.add_argument('--data_directory', type=str, required=True, help='Directory where images are stored')
+    parser.add_argument('--matches_file', type=str, required=True, help='File containing matches')
+    parser.add_argument('--output_file', type=str, required=True, help='File to save unique labels')
+    parser.add_argument('--max_rows', type=int, help='Maximum number of rows to display', default=None)
+
+    results = load_data('/Users/zz/Desktop/models/research/delf/delf/python/delg/delg_test/results/test/best_matches.txt', '/Users/zz/Desktop/models/research/delf/delf/python/delg/delg_test/results/test/index_image_to_landmark.csv', '/Users/zz/Desktop/models/research/delf/delf/python/delg/delg_test/results/test/index_label_to_category.csv')
+
+    args = parser.parse_args()
+
+    labels = set()
+    for result in results:
+        labels.add(result[1])
+
+    with open(args.output_file, 'w') as f:
+        for label in sorted(labels):
+            f.write(label + '\n')
+
+    display_images_with_labels(args.data_directory, args.matches_file, results, args.max_rows)
+
+    print(f"Labels have been extracted and saved to {args.output_file}.")
+
+if __name__ == '__main__':
+    main()
+
